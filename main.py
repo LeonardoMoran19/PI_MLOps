4222d92254ae037b714f8b1865d18afbbfe1e9a7
Leonardo Moran <oel.espol@gmail.com>
2024-03-05 09:38:13 -0500

    Prueba FastAPI

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/pruebaGet")
+def mostrar():
+    return 'Prueba funcionamiento'
+
+app.post("/pruebaPost")
+def conseguir(str :str):
+    return {'message':f'str: {str} '}
