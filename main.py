c134a03e46847e8e734a9a61de4d1ade544fc775
Leonardo Moran <oel.espol@gmail.com>
2024-03-12 14:21:24 -0500

    Cambio de metodo de funciones y archivos

--- main.py
+++ main.py
@@ -5,90 +5,51 @@ import pickle
 
 app = FastAPI()
 
-@app.get("/UsersRecommend")
-def UsersRecommend( year : int ):
-    user_reviews = pd.read_parquet('user_reviews.parquet')
-    steam_games = pd.read_parquet('steam_games.parquet')
-    df = user_reviews[(user_reviews['date'].dt.year == year) & 
-                      (user_reviews['sentiment_analysis'] > 0)].drop(columns=['date','sentiment_analysis'])
-    df = df.groupby('item_id')['recommend'].sum().reset_index()
-    df = df.sort_values(by='recommend', ascending=False)
-    df = df.head(3)
-    ids = df['item_id'].values
-    names =[]
-    for id in ids:
-        id = int(id)
-        names.append(steam_games.loc[id]['name'])
-    return 'Puesto 1: '+names[0]+', Puesto 2: '+names[1]+', Puesto 3: '+names[2]
-
-@app.get("/UsersNotRecommend")
-def UsersNotRecommend( year : int ):
-    user_reviews = pd.read_parquet('user_reviews.parquet')
-    steam_games = pd.read_parquet('steam_games.parquet')
-    df = user_reviews[(user_reviews['date'].dt.year == year) & 
-                      (user_reviews['sentiment_analysis'] == 0)&
-                      (user_reviews['recommend'] == 0) ].drop(columns=['recommend','date','sentiment_analysis'])
-    df = df['item_id'].value_counts().head(10).reset_index()
-    ids = df['item_id'].values
-    names =[]
-    for id in ids:
-        id = int(id)
-        try:
-            names.append(steam_games.loc[id]['name'])
-        except KeyError:
-            pass
-    return 'Puesto 1: '+names[0]+', Puesto 2: '+names[1]+', Puesto 3: '+names[2]
-
-@app.get("/SentimentAnalysis")
-def sentiment_analysis( year : int ):
-    df = pd.read_parquet('user_reviews.parquet')
-    df = df[(df['date'].dt.year == year)].drop(columns={'date','recommend','item_id'})
-    df = df.value_counts().reset_index().set_index('sentiment_analysis')
-    return 'Negativo = '+str(df.loc[0]['count'])+' Neutral = '+str(df.loc[1]['count'])+' Positivo = '+str(df.loc[2]['count'])
+playtime_genre = pd.read_parquet('playtimegenre.parquet')
+user_genre = pd.read_parquet('userforgenre.parquet')
+users_recommend = pd.read_parquet('usersrecommend.parquet')
+users_nrecommend = pd.read_parquet('usersnotrecommend.parquet')
+sentiment = pd.read_parquet('sentiment_analysis.parquet')
+recomendations = pd.read_parquet('recommendations.parquet')
 
 @app.get('/PlayTimeGenre')
 def PlayTimeGenre(genero: str):
-    steam_games = pd.read_parquet('steam_games.parquet', columns=['genres', 'date'])
-    user_items = pd.read_parquet('users_items.parquet', columns=['item_id', 'playtime'])
-    steam_games = steam_games[steam_games['genres'].apply(lambda x: genero.capitalize() in x)]
-    user_items = user_items.groupby('item_id')['playtime'].sum()
-    merged_df = steam_games.merge(user_items, how='left', left_index=True, right_index=True)
-    merged_df['playtime'] = merged_df['playtime'].fillna(0)
-    merged_df['date'] = pd.to_datetime(merged_df['date']).dt.year
-    merged_df = merged_df.groupby('date')['playtime'].sum().reset_index()
-    merged_df = merged_df.sort_values(by='playtime', ascending=False)
-    most_played_year = int(merged_df.iloc[0]['date'])
+    try:
+        return playtime_genre.loc[genero.lower()]['return']
+    except KeyError:
+        return 'Genero no se encuentra en los registros'
     
-    return f"Año de lanzamiento con más horas jugadas para el género {genero} es el: {most_played_year}"
-
 @app.get("/UserForGenre")
 def UserForGenre(genero: str):
-    steam_games = pd.read_parquet('steam_games.parquet', columns=['genres', 'date'])
-    user_items = pd.read_parquet('users_items.parquet', columns=['user_id', 'item_id', 'playtime'])
-    steam_games = steam_games[steam_games['genres'].apply(lambda x: genero.capitalize() in x)]
-    steam_games['date'] = pd.to_datetime(steam_games['date']).dt.year
-    indices = steam_games.index
-    user_items = user_items[user_items['item_id'].isin(indices)]
-    user_top = user_items.groupby('user_id')['playtime'].sum().reset_index().sort_values(by='playtime', ascending=False)
-    user_top_id = user_top.iloc[0]['user_id']
-    user = user_items[user_items['user_id'] == user_top_id]
-    user = user.set_index('item_id').merge(steam_games.drop(columns=['genres']), how='left', left_index=True, right_index=True)
-    user = user.drop(columns=[ 'user_id']).groupby('date')['playtime'].sum().reset_index()
-    user = user[user['playtime'] > 0]
-    texto = f'Usuario con más horas jugadas para el género {genero}: {user_top_id}. Horas jugadas por año de lanzamiento: '
-    for index, row in user.iterrows():
-        texto += f'Año {int(row["date"])}: {int(row["playtime"])} hora(s). '
+    try:
+        return user_genre.loc[genero.lower()]['return']
+    except KeyError:
+        return 'Genero no se encuentra en los registros'
 
-    return texto
+@app.get("/UsersRecommend")
+def UsersRecommend( year : int ):
+    try:
+        return users_recommend.loc[year]['return']
+    except KeyError:
+        return 'Año no se encuentra en los registros'
+
+@app.get("/UsersNotRecommend")
+def UsersNotRecommend( year : int ):
+    try:
+        return users_nrecommend.loc[year]['return']
+    except KeyError:
+        return 'Año no se encuentra en los registros'
+
+@app.get("/SentimentAnalysis")
+def sentiment_analysis( year : int ):
+    try:
+        return sentiment.loc[year]['return']
+    except KeyError:
+        return 'Año no se encuentra en los registros'
 
 @app.get("/GameRecommendation")
 def recomendacion_juego( id :int):
-    steam_games=pd.read_parquet('steam_games.parquet').drop(columns={'date'}) 
-    with open('similarity_matrix.pkl', 'rb') as f:
-        similarity_matrix = pickle.load(f)
-    game_index = steam_games.index.get_loc(id)
-    similar_games_indices = similarity_matrix[game_index].argsort()[::-1][1:6]
-    similar_games = steam_games.iloc[similar_games_indices]
-    similar_games = similar_games['name'].values
-    
-    return 'Juegos recomendados: 1.'+similar_games[0]+', 2.'+similar_games[1]+', 3.'+similar_games[2]+', 4.'+similar_games[3]+', 5.'+similar_games[4]
+    try:
+        return recomendations.loc[id]['return']
+    except KeyError:
+        return 'Juego no se encuentra en los registros'
